64e3ac9d0cc112784dcbdbe64ce7c451b54d6a76
Christof Kälin <christof.kaelin@protonmail.com>
2023-12-17 21:11:25 +0100

    hangman added

---
+++ hangman/hangman.py
@@ -0,0 +1,49 @@
+# Step 5
+from typing import List
+import hangman_art
+import random
+import hangman_words
+import os
+
+chosen_word = random.choice(hangman_words.word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+print(hangman_art.logo)
+
+# Create blanks
+display: List = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    os.system('clear')
+
+    if guess in display:
+        print(f"You've already guessed {guess}")
+    # Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+
+        if letter == guess:
+            display[position] = letter
+
+    # Check if user is wrong.
+    if guess not in chosen_word:
+        print(f"Buggers! Letter {letter} is not in the word, you lose a life!")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    # Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print(hangman_art.stages[lives])
