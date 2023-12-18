83e49278c4141f45cac7da5a5c4f1ed2e58e8090
Christof Kälin <christof.kaelin@pm.me>
2023-12-18 14:08:59 +0100

    caesar's v.3

---
+++ caesars/cipher.py
@@ -0,0 +1,22 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def caesar(text, shift, direction):
+  result = ""
+  
+  for char in text:
+    position= alphabet.index(char)
+    shift_amount = shift
+    if direction == "decode":
+      shift_amount = shift * -1
+    new_position = (position + shift_amount) % len(alphabet)
+    result += alphabet[new_position]
+    
+  print(f"The {direction}d text is {result}")
+
+caesar(text, shift, direction)
+
+exit(0)
