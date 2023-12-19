4ab0f06a91e0d26c3959ebbe412e258bc436cbd1
Christof Kälin <christof.kaelin@pm.me>
2023-12-19 18:37:58 +0100

    blind-auction added

---
+++ blind-auction/main.py
@@ -0,0 +1,23 @@
+import os
+from art import logo
+
+bids = {}
+
+
+print(logo)
+
+bidding = True
+
+while bidding:
+  name = input("What is your name?: ")
+  bid = input("What's your bid?: ")
+  os.system('clear')
+  bids[name] = bid
+  should_continue = input("Are there more bidders (yes/no)?: ")
+  if should_continue == "no":
+    bidding = False
+
+max_bid = max(bids.values())
+for key, value in bids.items():
+  if value == max_bid:
+    print(f"The highest bid of {value} came from {key}!")
