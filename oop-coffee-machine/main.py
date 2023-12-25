befaf6f784d68ba15d6eb6433ecafd4423272a80
Christof Kälin <christof.kaelin@protonmail.com>
2023-12-25 18:46:05 +0100

    oop coffee machine stub

---
+++ oop-coffee-machine/main.py
@@ -0,0 +1,22 @@
+from art import logo
+from menu import menu
+
+
+def main_loop():
+    print("Welcome! Please choose drink:")
+
+
+class Cashier:
+    def __init__():
+        return True
+
+
+class Machine:
+    def __init__():
+        return True
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(logo)
+    main_loop()
