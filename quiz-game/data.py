dd791efa81d1165779bf9b75717ec47812e5eba3
Christof Kälin <christof.kaelin@protonmail.com>
2023-12-27 12:42:33 +0100

    quiz game added (questions from open trivia db API)

---
+++ quiz-game/data.py
@@ -0,0 +1,118 @@
+question_data = [
+    {
+        "type": "multiple",
+        "difficulty": "easy",
+        "category": "Science: Computers",
+        "question": "How long is an IPv6 address?",
+        "correct_answer": "128 bits",
+        "incorrect_answers": [
+                "32 bits",
+                "64 bits",
+                "128 bytes"
+        ]
+    },
+    {
+        "type": "multiple",
+        "difficulty": "medium",
+        "category": "Science: Computers",
+        "question": "Which coding language was the #1 programming language in terms of usage on GitHub in 2015?",
+        "correct_answer": "JavaScript",
+        "incorrect_answers": [
+                "C#",
+                "Python",
+                "PHP"
+        ]
+    },
+    {
+        "type": "multiple",
+        "difficulty": "hard",
+        "category": "Science: Computers",
+        "question": "The Harvard architecture for micro-controllers added which additional bus?",
+        "correct_answer": "Instruction",
+        "incorrect_answers": [
+                "Address",
+                "Data",
+                "Control"
+        ]
+    },
+    {
+        "type": "multiple",
+        "difficulty": "hard",
+        "category": "Science: Computers",
+        "question": "Which RAID array type is associated with data mirroring?",
+        "correct_answer": "RAID 1",
+        "incorrect_answers": [
+                "RAID 0",
+                "RAID 10",
+                "RAID 5"
+        ]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Science: Computers",
+        "question": "RAM stands for Random Access Memory.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+                "False"
+        ]
+    },
+    {
+        "type": "multiple",
+        "difficulty": "medium",
+        "category": "Science: Computers",
+        "question": "What does the acronym CDN stand for in terms of networking?",
+        "correct_answer": "Content Delivery Network",
+        "incorrect_answers": [
+                "Content Distribution Network",
+                "Computational Data Network",
+                "Compressed Data Network"
+        ]
+    },
+    {
+        "type": "multiple",
+        "difficulty": "hard",
+        "category": "Science: Computers",
+        "question": "Who is the founder of Palantir?",
+        "correct_answer": "Peter Thiel",
+        "incorrect_answers": [
+                "Mark Zuckerberg",
+                "Marc Benioff",
+                "Jack Dorsey"
+        ]
+    },
+    {
+        "type": "multiple",
+        "difficulty": "medium",
+        "category": "Science: Computers",
+        "question": "Whistler was the codename of this Microsoft Operating System.",
+        "correct_answer": "Windows XP",
+        "incorrect_answers": [
+                "Windows 2000",
+                "Windows 7",
+                "Windows 95"
+        ]
+    },
+    {
+        "type": "multiple",
+        "difficulty": "easy",
+        "category": "Science: Computers",
+        "question": "What does the &quot;MP&quot; stand for in MP3?",
+        "correct_answer": "Moving Picture",
+        "incorrect_answers": [
+                "Music Player",
+                "Multi Pass",
+                "Micro Point"
+        ]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "hard",
+        "category": "Science: Computers",
+        "question": "The IBM PC used an Intel 8008 microprocessor clocked at 4.77 MHz and 8 kilobytes of memory.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+                "True"
+        ]
+    }
+]
