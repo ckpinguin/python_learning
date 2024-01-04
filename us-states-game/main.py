624a366c5568e92388163f29f7d8cd13ba3f2b8a
Christof Kälin <christof.kaelin@pm.me>
2024-01-04 20:53:21 +0100

    started us states quiz project

---
+++ us-states-game/main.py
@@ -0,0 +1,20 @@
+import pandas
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+
+grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+print(grey_squirrels_count)
+print(red_squirrels_count)
+print(black_squirrels_count)
+
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrels_count, red_squirrels_count, black_squirrels_count]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_color_count.csv")
+# print(fur_color.groupby("Primary Fur Color").count())
