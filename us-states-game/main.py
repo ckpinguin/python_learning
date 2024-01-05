277a2e8ad463debedd6c3e2fb1ee0d074c371bd6
Christof Kälin <christof.kaelin@pm.me>
2024-01-05 11:39:33 +0100

    version without list comprehensions

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -1,18 +1,49 @@
 import turtle
+import pandas
+
+# Howto get coords for the input file:
+""" def get_mouse_click_coor(x, y):
+        print(x, y)
+    turtle.onscreenclick(get_mouse_click_coor) """
 
 screen = turtle.Screen()
 screen.title("U.S. States game")
 
+
+FONT = ("Arial", 16, "normal")
 image = "./blank_states_img.gif"
 screen.addshape(image)
+# background = turtle.Turtle()
+# background.shape(image)
+# Simpler than using a turtle shape as background:
+screen.bgpic(image)
 
-turtle.shape(image)
-answer_state = screen.textinput(
-    title="Guess the State", prompt="What's another state's name?")
+state_writer = turtle.Turtle()
+state_writer.penup()
+state_writer.hideturtle()
 
-""" def get_mouse_click_coor(x, y):
-    print(x, y)
-turtle.onscreenclick(get_mouse_click_coor) """
+df = pandas.read_csv('50_states.csv')
+all_states = df.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < df.state.size:
+    answer_state = screen.textinput(
+        title=f"{len(guessed_states)}/{df.state.size}", prompt="What's another state's name? (Type Exit to stop)").title()
+
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        df = pandas.DataFrame(missing_states)
+        df.to_csv("states_to_learn.csv")
+        break
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        state_data = df[df.state == answer_state]
+        state_writer.goto(int(state_data.x), int(state_data.y))
+        state_writer.write(state_data.state.item(), align="center", font=FONT)
 
 
 turtle.mainloop()
