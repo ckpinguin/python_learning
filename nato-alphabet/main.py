ab36b6fd8fcf133151b5f5782738206ae28454d6
Christof Kälin <christof.kaelin@pm.me>
2024-01-05 18:25:14 +0100

    nato alphabet experiments with pandas and dict comprehension

---
+++ nato-alphabet/main.py
@@ -0,0 +1,37 @@
+import pandas
+""" student_dict = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 76, 98]
+}
+
+# Looping through dictionaries:
+for (key, value) in student_dict.items():
+    # Access key and value
+    pass
+
+student_data_frame = pandas.DataFrame(student_dict)
+
+# Loop through rows of a data frame
+for (index, row) in student_data_frame.iterrows():
+    # Access index and row
+    # Access row.student or row.score
+    pass """
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+# TODO 1. Create a dictionary in this format:
+# {"A": "Alfa", "B": "Bravo"}
+df = pandas.read_csv('nato_phonetic_alphabet.csv')
+# print(df)
+# df_dict = df.to_dict()
+# print(df_dict)
+alphabet_dict = {row.letter: row.code for (_, row) in df.iterrows()}
+print(alphabet_dict)
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+name = input('Enter a name: ').upper()
+
+for letter in name:
+    print(letter)
+    print(alphabet_dict[letter])
