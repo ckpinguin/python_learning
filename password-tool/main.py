8b40bac0465f977c4b5515b17757a7eda93d4cc6
Christof Kälin <christof.kaelin@pm.me>
2024-01-08 17:31:50 +0100

    canvas added

--- password-tool/main.py
+++ password-tool/main.py
@@ -1,4 +1,5 @@
-from tkinter import Tk, PhotoImage, Label, Canvas
+from tkinter import Tk, PhotoImage, Canvas
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -10,8 +11,11 @@ window.title("Password Manager")
 window.config(padx=20, pady=20)
 
 mypass_img = PhotoImage(file="logo.png")
-canvas = Canvas(width=200, height=200, highlightthickness=0)
-canvas.create_image(200, 189, image=mypass_img, )
-timer_text = canvas.create_text(
-    100, 130, text="", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas = Canvas(height=200, width=200, highlightthickness=0)
+canvas.create_image(100, 100, image=mypass_img)
+# timer_text = canvas.create_text(
+#    100, 130, text="", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=1, column=1)
+
+
+window.mainloop()
