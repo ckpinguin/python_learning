f78f34cb588e5cc886f8523ff0b8c6996a72188d
Christof Kälin <christof.kaelin@pm.me>
2024-01-09 10:21:29 +0100

    error catching and cleaner code

--- nato-alphabet/main.py
+++ nato-alphabet/main.py
@@ -20,18 +20,28 @@ for (index, row) in student_data_frame.iterrows():
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-# TODO 1. Create a dictionary in this format:
 # {"A": "Alfa", "B": "Bravo"}
 df = pandas.read_csv('nato_phonetic_alphabet.csv')
 # print(df)
 # df_dict = df.to_dict()
 # print(df_dict)
 alphabet_dict = {row.letter: row.code for (_, row) in df.iterrows()}
-print(alphabet_dict)
+# print(alphabet_dict)
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-name = input('Enter a name: ').upper()
 
-output_list = [alphabet_dict[letter] for letter in name]
+def get_input():
+    word = input('Enter a name: ').upper()
+    return word
 
+
+def translate_to_nato(chars):
+    try:
+        output_list = [alphabet_dict[letter] for letter in chars]
+        return output_list
+    except KeyError:
+        print("Sorry, only letters allowed.")
+        return translate_to_nato(get_input())
+
+
+output_list = translate_to_nato(get_input())
 print(output_list)
