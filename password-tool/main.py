ce3c6fa465c8fb061737d5a08e36efdfafc1b5fc
Christof Kälin <christof.kaelin@pm.me>
2024-01-09 12:37:16 +0100

    search functionality and less popup windows, instead using label with timeouts (still buggy, when multiple timeouts are running)

--- password-tool/main.py
+++ password-tool/main.py
@@ -7,12 +7,17 @@ import json
 PASSWORD_FILE = "passwords.json"
 DEFAULT_EMAIL = "ck@pm.me"
 FONT_NAME = "Arial"
+DEFAULT_FONT = (FONT_NAME, 10, "normal")
+DEFAULT_MSG = ""
+MESSAGE_TIMEOUT_MS = 5000
 
 
 PINK = "#e2979c"
 RED = "#e7305b"
-GREEN = "#9bdeac"
+GREEN = "#0a6522"
 YELLOW = "#f7f5dd"
+BLACK = "#000000"
+WHITE = "#ffffff"
 
 # ---------------- PASSWORD GENERATOR --------------- #
 
@@ -66,7 +71,7 @@ def save_password():
         if is_ok:
             try:
                 with open(PASSWORD_FILE, "r") as file:
-                    data = json.load(file)
+                    data: dict = json.load(file)
                     # Not just appending, we need a valid json structure
                     data.update(new_data)
             except FileNotFoundError:
@@ -86,64 +91,99 @@ def write_json(data):
     with open(PASSWORD_FILE, "w") as file:
         json.dump(data, file, indent=4)
 
+
+def search_password(identifier):
+    try:
+        with open(PASSWORD_FILE, "r") as file:
+            data = json.load(file)
+            update_loaded_fields(data[identifier])
+
+    except FileNotFoundError:
+        alert_msg("No password file found.\nPlease add some passwords first.")
+    except KeyError:
+        alert_msg(f"No password found for {identifier}.")
+    finally:
+        pyperclip.copy(password_entry.get())
+
+
+def update_loaded_fields(record):
+    username_entry.delete(0, END)
+    username_entry.insert(0, record["email"])
+    password_entry.delete(0, END)
+    password_entry.insert(0, record["password"])
 # ------------------ UI SETUP --------------------- #
 
 
 def reset_msg():
-    message_label.config(text="")
+    message_label.config(text=DEFAULT_MSG, fg=BLACK, font=DEFAULT_FONT)
 
 
-def alert_msg(text):
+def alert_msg(text, timeout=MESSAGE_TIMEOUT_MS):
     message_label.config(text=text,
-                         fg=RED, font=(FONT_NAME, 18, "bold"))
-    messagebox.showerror(title="ERROR", message=text)
+                         fg=RED, font=(FONT_NAME, 12, "bold"))
+    message_label.after(timeout, reset_msg)
 
 
-def info_msg(text):
+def info_msg(text, timeout=MESSAGE_TIMEOUT_MS):
     message_label.config(text=text,
-                         fg=GREEN, font=(FONT_NAME, 18, "normal"))
-    messagebox.showinfo(title="INFO", message=text)
+                         fg=GREEN, font=(FONT_NAME, 12, "normal"))
+    message_label.after(timeout, reset_msg)
 
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(heigh=220, padx=20, pady=20)
 
+
+# Row 0
+message_label = Label(text=DEFAULT_MSG, font=DEFAULT_FONT)
+message_label.grid(row=0, column=0, columnspan=3)
+
+# Row 1
 mypass_img = PhotoImage(file="logo.png")
 canvas = Canvas(height=200, width=200, highlightthickness=0)
-canvas.create_image(120, 100, image=mypass_img)
+canvas.create_image(100, 100, image=mypass_img)
 # timer_text = canvas.create_text(
 #    100, 130, text="", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.grid(row=0, column=1)
+canvas.grid(row=1, column=1)
 
 
-message_label = Label(text="", font=(FONT_NAME))
-message_label.grid(row=0, column=0)
-
-website_label = Label(text="Website:", font=(FONT_NAME))
-website_label.grid(row=1, column=0)
+# Row 2
+website_label = Label(text="Website:", font=DEFAULT_FONT)
+website_label.grid(row=2, column=0)
 website = StringVar()
-website_entry = Entry(width=35, textvariable=website)
-website_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
+website_entry = Entry(width=35, textvariable=website, font=DEFAULT_FONT)
+website_entry.grid(row=2, column=1, columnspan=1, sticky="EW")
 
-username_label = Label(text="Email/Username:", font=(FONT_NAME))
-username_label.grid(row=2, column=0)
+search_button = Button(text="Search", highlightthickness=0, font=DEFAULT_FONT,
+                       command=lambda: search_password(website_entry.get()))
+search_button.grid(row=2, column=2, sticky="EW")
+window.bind('<Return>', lambda event: search_password(website_entry.get()))
+
+
+# Row 3
+username_label = Label(text="Email/Username:", font=DEFAULT_FONT)
+username_label.grid(row=3, column=0)
 username = StringVar()
-username_entry = Entry(width=35, textvariable=username)
+username_entry = Entry(width=35, textvariable=username, font=DEFAULT_FONT)
 username_entry.insert(0, DEFAULT_EMAIL)
-username_entry.grid(row=2, column=1, columnspan=2, sticky="EW")
+username_entry.grid(row=3, column=1, columnspan=2, sticky="EW")
 
 
-password_label = Label(text="Password:", font=(FONT_NAME))
-password_label.grid(row=3, column=0)
+# Row 4
+password_label = Label(text="Password:", font=DEFAULT_FONT)
+password_label.grid(row=4, column=0)
 password = StringVar()
-password_entry = Entry(width=21, textvariable=password)
-password_entry.grid(row=3, column=1)
+password_entry = Entry(width=21, textvariable=password, font=DEFAULT_FONT)
+password_entry.grid(row=4, column=1)
 password_button = Button(text="Generate Password", command=generate_password)
-password_button.grid(row=3, column=2, sticky="EW")
+password_button.grid(row=4, column=2, sticky="EW")
 
+
+# Row 5
 add_button = Button(text="Add", width=36,
-                    highlightthickness=0, command=save_password)
-add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
+                    highlightthickness=0, command=save_password, font=DEFAULT_FONT)
+add_button.grid(row=5, column=1, columnspan=2, sticky="EW")
+
 
 window.mainloop()
